68ad20317e133f1da142fd61e8d7a21a9fdcb47d
rimanaru <rima.naruseviciute@gmail.com>
2018-02-23 20:39:31 +0200

    nd

--- interfeisai_abstrakcios11/index.php
+++ interfeisai_abstrakcios11/index.php
@@ -1,93 +1,81 @@
 <?php
+class Marks{
+    public $student_no=null;
+    public $module_code=null;
+    public $mark=null;
+    
+    function __construct($student_no,$module_code,$mark ){
+        $this->student_no=$student_no;
+        $this->module_code=$module_code;
+        $this->mark=$mark;
+    }
+    public function save(){
+        $pdo = new PDO("mysql:host=localhost;dbname=namu_darbu_baze", "root","");
+        $sql = "INSERT INTO marks (student_no, module_code, mark) VALUES (:student_no, :module_code,:mark)";
+        $sth = $pdo->prepare($sql);
+      
+        $sth->bindParam(':student_no', $this->student_no);
+        $sth->bindParam(':module_code', $this->module_code);
+        $sth->bindParam(':mark', $this->mark);
+        
+      
+        $sth->execute();  
+    }
+    }
+    $marks=new Marks('qqq','ccc','kkk');
+    var_dump($marks);
+    $marks->save($marks);
 
+class Modules {
+    public $module_code=null;
+    public $module_name=null;
+    function __construct($module_code, $module_name){
+        $this->module_code=$module_code;
+        $this->module_name=$module_name;
+    }
+    public function save(){
+        $pdo = new PDO("mysql:host=localhost;dbname=namu_darbu_baze", "root","");
+        $sql = "INSERT INTO modules (module_code, module_name,) VALUES (:module_code,:module_name)";
+        $sth = $pdo->prepare($sql);
+      
+        $sth->bindParam(':module_code', $this->module_code);
+        $sth->bindParam(':module_name', $this->module_name);
+      
+        $sth->execute();   
+    }
 
-   $host = "localhost";
-   $user = "root";
-   $password = "";
-   $db = "namu_darbu_baze";
-   $dsn = "mysql:host=$host;dbname=$db";
-   $pdo = new PDO($dsn, $user, $password);
-   $sql = " SELECT * FROM `modules`";
-   $module= $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-    //var_dump($module);
-     echo '<pre>';
-     print_r($module);
-     echo '<pre>';
-
-     $sql = " SELECT * FROM `students`";
-     $student= $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-      //var_dump($module);
-       echo '<pre>';
-       print_r($student);
-       echo '<pre>';
-
-       $sql = " SELECT * FROM `marks`";
-       $mark= $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-        //var_dump($module);
-         echo '<pre>';
-         print_r($mark);
-         echo '<pre>';
-   // pagalvojau kad sita reikia atsisiusti kad ten galeciau deti naujus irasus, kuriuos sukursiu
-   // su objektais(new students irtt, bet nezinau kaip sita atsiusta array ikist i mano classes,
-   // turbut i class Data paveldeti ir papildyt )
-    class Data{
-        function getModule(){
-  }
 }
-class Students extends Data{
+
+$modules=new Modules('rrr','fff');
+var_dump($modules);
+$modules->save($modules);
+
+class Students {
 public $student_no=null;
 public $surname=null;
 public $forename=null;
-
 
 function __construct($student_no,$surname,$forename ){
    $this->student_no=$student_no;
    $this->surname=$surname;
    $this->forename=$forename;
 }
-
 public function save(){
+    $pdo = new PDO("mysql:host=localhost;dbname=namu_darbu_baze", "root","");
     $sql = "INSERT INTO students (student_no, surname, forename) VALUES (:student_no, :surname, :forename)";
     $sth = $pdo->prepare($sql);
-    //$sth->execute($kazka ka sukursiu)
-
-    //sito pagalvojau reikia kad nusiusti i duomenu baze, bet cia turbut gausis kaskart 
-    //vis prisijungti prie db, tai turbut reikia ji deti i class Data
+  
+    $sth->bindParam(':student_no', $this->student_no);
+    $sth->bindParam(':surname', $this->surname);
+    $sth->bindParam(':forename', $this->forename);
+    $sth->execute();      
 }
-
 }
 $student=new Students('mmm','lll','sss');
-echo $student->module;
 
-class Modules extends Data{
-    public $Module_code=null;
-    public $Module_name=null;
-    function __construct($Module_code,$Module_name ){
-        $this->Module_code=$Module_code;
-        $this->Module_name=$Module_name;
-    }
-    public function save(){
-        
-    }
+$student->save($student);
 
-}
 
-$module=new Modules('rrr','fff','ddd');
 
-class Marks{
-public $student_no=null;
-public $module_code=null;
-public $mark=null;
-function __construct($student_no,$module_code,$mark ){
-    $this->student_no=$student_no;
-    $this->module_code=$module_code;
-    $this->mark=$mark;
-}
-public function save(){
-    
-}
-}
-
-$module=new Marks('qqq','ccc','kkk');
 
 ?>
